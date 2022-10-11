59f2beaf62328bf6d2e87a8731790663f1de87b3
xxx <humengmingxx@gmail.com>
2022-10-11 22:59:08 +0800

    feat：init menu

--- docs/.vitepress/constants.ts
+++ docs/.vitepress/constants.ts
@@ -1,2 +1,2 @@
 export const WA_NAME = 'Wa'
-export const WA_DESC = '凹语言™（凹读音“Wa”）是针对 WASM 平台设计的通用编程语言，同时支持 Linux、macOS 和 Windows 等主流操作系统和 Chrome 等浏览器环境，同时也支持作为独立Shell脚本和被嵌入脚本模式执行。'
+export const WA_DESC = '针对 WASM 平台设计的通用编程语言'
