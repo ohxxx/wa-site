aa5095ee327f8f357d6c5f2ccdbfb473a0a39545
xxx <humengmingxx@gmail.com>
2022-10-12 13:56:37 +0800

    feat：add core team members

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,14 @@
 import { h } from 'vue'
 import Theme from 'vitepress/theme'
 import '../styles/vars.css'
+import HomePage from '../components/HomePage.vue'
 import SvgImage from '../components/SvgImage.vue'
 
 export default {
   ...Theme,
   Layout() {
     return h(Theme.Layout, null, {
+      'home-features-after': () => h(HomePage),
     })
   },
   enhanceApp({ app }) {
